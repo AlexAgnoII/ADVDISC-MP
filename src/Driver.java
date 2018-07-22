8efce26c4ca304af0ef171d1046d250ca4b51cd8
AlexAgnoII <3alex23@gmail.com>
2018-07-22 22:57:57 +0800

    v1.0
    - Added Gauss_Jordan and span functions for vector class.
    - included one of test function inside main
    - Gauss_Jordan may still have problem:
       a) Needs to take into account all non-zero elements once it reaches the end diagonal and its element is 0. (So, it needs to look up on its column and swap the first row found that has non-zero element on the same index. Afterwards, set that row as new current index.)
       b) Might cause infinite loop? (Still need to see if it'll do)
      c)more t e s t i n g

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,36 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mp1.Vector;
 
 public class Driver {
 	
+	public static void gauss_Jordan_test() {
+        Double[] test1 = {1.0, 1.0, 2.0};
+        Double[] test2 = {2.0, 4.0, -3.0};
+        Double[] test3 = {3.0, 6.0, -5.0};
+        Double[] testConstraint = {9.0, 1.0, 0.0};
+
+        Vector v1 = new Vector(test1, test1.length);
+        Vector v2 = new Vector(test2, test2.length);
+        Vector v3 = new Vector(test3, test3.length);
+        Vector constraints = new Vector(testConstraint, 3);
+
+        List<Vector> vecList = new ArrayList<Vector>();
+
+        vecList.add(v1);
+        vecList.add(v2);
+        vecList.add(v3);
+
+        Vector.Gauss_Jordan(vecList, 3, constraints);
+//        int theSpan = Vector.span(vecList, 3);
+//        System.out.println("The vecctors spans as: " + theSpan + "d");
+    }
+	
 	//for testing
 	public static void main(String[] args) {
-			
+		gauss_Jordan_test();
 	}
 }
--- src/com/mp1/Vector.java
+++ src/com/mp1/Vector.java
@@ -1,5 +1,6 @@
 package com.mp1;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,13 +13,16 @@ public class Vector {
 
     private Double[] data;
     private int dimension;
+    
+    private static final Double D_ZERO = 0.0;
+    private static final Double D_ONE = 1.0;
 
     public Vector(int dimension) {
         this.dimension = dimension;
         this.data = new Double[dimension];
 
         for(int i = 0; i < dimension; i++) {
-            this.data[i] = 2.0;
+            this.data[i] = 0.0;
         }
     }
 
@@ -47,43 +51,209 @@ public class Vector {
     	}
         return this;
     }
-   
-    public static Vector Gauss_Jordan(List<Vector> vectors,
-                                      int dimension,
-                                      Vector constants) {
 
-        return new Vector(1); // temp
-    }
+	public static Vector Gauss_Jordan(List<Vector> vectors, int dimension, // dimension of all vectors in vectors list.
+			Vector constants) {
 
-    public static int span(List<Vector> vectors, int dimensions) {
+		REF(vectors, dimension, constants);
+		RREF(vectors, dimension, constants);
 
-        return 1;
-    }
+		System.out.println("Final: ");
+		printAllVectors(vectors);
+		System.out.print("Constants: ");
+		constants.printElements();
 
-    /**
-     *
-     * @param index - the position of the number in the vector.
-     * @return the number in that given index.
-     */
-    public Double getElement(int index) {
-        return this.data[index];
-    }
+		return constants;
+	}
 
-    public int getDimension() {
-        return this.dimension;
-    }
-    
-    public void printElements() {
-        System.out.print("[");
+	private static void REF(List<Vector> vectors, int dimension, // dimension of all vectors in vectors list.
+			Vector constants) {
+		int vectListSize = vectors.size();
 
-        for(int i = 0; i < this.dimension; i++) {
-            System.out.print(this.data[i]);
-            if (i + 1 != this.dimension) {
-                System.out.print(" ");
-            }
-        }
+		System.out.println("Initial: ");
+		printAllVectors(vectors);
+		System.out.print("Constants: ");
+		constants.printElements();
+		System.out.println();
 
-        System.out.print("]");
-    }
+		// row echelon form
+		for (int currentIndex = 0; currentIndex < vectListSize; currentIndex++) {
+			Double currentElement = vectors.get(currentIndex).data[currentIndex];
+			System.out.println("Row[" + currentIndex + "] Current element: " + currentElement);
+
+			// check if current element 0.
+			if (currentElement.compareTo(D_ZERO) == 0) { // if 0, we have to swap.
+				System.out.println("its 0!");
+				int nextIndex = findNonZero(vectors, vectListSize, currentIndex, currentElement, constants);
+
+				if (nextIndex != -1) {
+					swapRows(vectors, currentIndex, nextIndex, constants);
+				}
+
+				currentElement = vectors.get(currentIndex).data[currentIndex];
+				System.out.println("New Row[" + currentIndex + "] Current element: " + currentElement);
+
+				printAllVectors(vectors);
+				System.out.print("Constants: ");
+				constants.printElements();
+			}
+
+			// check if current element 1.
+			if (currentElement.compareTo(D_ONE) != 0) { // if greater we have to scale vector by dividing it to become 1.
+				System.out.println("its greater than 1 OR its negative!!");
+				divideRow(vectors, dimension, currentIndex, currentElement, constants);
+				printAllVectors(vectors);
+				System.out.print("Constants: ");
+				constants.printElements();
+			}
+
+			else if (currentElement.compareTo(D_ZERO) == 0.0) {
+				System.out.println("Its still zero!");
+			}
+
+			// make all elements below currentElement, value 1, as 0. (vectListSize - 1
+			// because last element diagonal has nothing below it.
+			if (currentIndex + 1 != vectListSize) { // if last row, don't do anything.
+
+				for (int succeedingIndex = currentIndex + 1; succeedingIndex < vectListSize; succeedingIndex++) {
+					addRows(vectors, currentIndex, succeedingIndex, dimension, constants);
+				}
+
+			}
+
+			System.out.println();
+		}
+	}
+
+	private static void RREF(List<Vector> vectors, int dimension, // dimension of all vectors in vectors list.
+			Vector constants) {
+
+		for (int currentIndex = vectors.size() - 1; currentIndex > 0; currentIndex--) {
+			System.out.println(
+					"Row[" + currentIndex + "] Current element: " + vectors.get(currentIndex).data[currentIndex]);
+			for (int precedingIndex = currentIndex - 1; precedingIndex >= 0; precedingIndex--) {
+				addRows(vectors, currentIndex, precedingIndex, dimension, constants);
+			}
+
+		}
+	}
+
+	public static int span(List<Vector> vectors, int dimensions) {
+		Vector vector = new Vector(dimensions);
+		Boolean found = false;
+		int count = 0;
+		int index = 0;
+
+		Gauss_Jordan(vectors, dimensions, vector);
+		System.out.println("Span mode: ");
+
+		for (int currentIndex = 0; currentIndex < vectors.size(); currentIndex++) {
+			index = 0;
+			found = false;
+			while (index < dimensions && !found) {
+				if (vectors.get(currentIndex).data[index].compareTo(D_ZERO) != 0) {
+					found = true;
+					count++;
+				}
+				index++;
+			}
+
+		}
+
+		return count;
+	}
+
+	/* Helper functions */
+
+	/* Find other nonzero element row when current element is 0 */
+	private static int findNonZero(List<Vector> vectors, int vectListSize, int currentIndex, Double currentElement,
+			Vector constants) {
+		Boolean found = false;
+		int nextIndex = currentIndex + 1;
+
+		// check if there exists a nonzero element on a vector below the current
+		// vector's element.
+		while (!found && nextIndex < vectListSize) {
+
+			if (currentElement.compareTo(vectors.get(nextIndex).data[currentIndex]) != 0) {
+				currentElement = vectors.get(nextIndex).data[currentIndex];
+				found = true;
+				System.out.println("Nonzero found!");
+			} else {
+				nextIndex++;
+			}
+		}
+
+		if (found == false) {// no swapping allowed.
+			System.out.println("Did not find any other nonzero..");
+			nextIndex = -1;
+		}
+
+		return nextIndex;
+	}
+
+	private static void swapRows(List<Vector> vectors, int currentIndex, int nextIndex, Vector constants) {
+		Collections.swap(vectors, currentIndex, nextIndex);
+		// swap constants
+		Double temp = constants.data[nextIndex];
+		constants.data[nextIndex] = constants.data[currentIndex];
+		constants.data[currentIndex] = temp;
+	}
+
+	/*
+	 * Used for gaus-jordan elimination: dividing row so that the current non-zero
+	 * element is 1.
+	 */
+	private static void divideRow(List<Vector> vectors, int dimension, int vecListIndex, Double currentElement,
+			Vector constants) {
+
+		Double mult = (1.0 / currentElement);
+
+		for (int i = 0; i < dimension; i++) {
+			if (vectors.get(vecListIndex).data[i].compareTo(D_ZERO) != 0)
+				vectors.get(vecListIndex).data[i] = vectors.get(vecListIndex).data[i] * mult;
+		}
+
+		// multiply to constant
+		if (constants.data[vecListIndex].compareTo(D_ZERO) != 0)
+			constants.data[vecListIndex] = constants.data[vecListIndex] * mult;
+
+	}
+
+	private static void addRows(List<Vector> vectors, int currentIndex, int otherIndex, int dimension,
+			Vector constants) {
+		System.out.println("Checking: " + vectors.get(otherIndex).data[currentIndex]);
+
+		if (vectors.get(otherIndex).data[currentIndex].compareTo(D_ZERO) != 0) {
+			Double mult = vectors.get(otherIndex).data[currentIndex];
+			for (int i = 0; i < dimension; i++) {
+				vectors.get(otherIndex).data[i] += -mult * vectors.get(currentIndex).data[i];
+			}
+
+			constants.data[otherIndex] += -mult * constants.data[currentIndex];
+		}
+
+		printAllVectors(vectors);
+		System.out.print("Constants: ");
+		constants.printElements();
+	}
+
+	public void printElements() {
+		System.out.print("[");
+		for (int i = 0; i < this.dimension; i++) {
+			System.out.print(data[i]);
+
+			if (i + 1 != dimension) {
+				System.out.print(" ");
+			}
+		}
+		System.out.print("]" + " Dimension: " + this.dimension + "\n");
+	}
+
+	private static void printAllVectors(List<Vector> vectors) {
+		for (int i = 0; i < vectors.size(); i++) { // checking onli.
+			vectors.get(i).printElements();
+		}
+	}
 
 }
